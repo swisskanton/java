f5cff7277e1ae96281fb6709f25f47591f68390d
swisskanton <kormanyos.antal@gmail.com>
2023-06-09 10:02:40 +0200

    application context tests

--- spring-di/src/test/java/com/springdi/SpringDiApplicationTests.java
+++ spring-di/src/test/java/com/springdi/SpringDiApplicationTests.java
@@ -1,10 +1,30 @@
 package com.springdi;
 
+import com.springdi.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringDiApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowireController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {

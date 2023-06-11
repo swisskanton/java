5f9892601b6b1f70e44fe6b2d272bf3526ad1746
swisskanton <kormanyos.antal@gmail.com>
2023-06-11 13:04:28 +0200

    there are two controls, one for the beer and one for the customers

---
+++ spring-6-rest-mvc/src/main/java/com/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,36 @@
+package com.spring6restmvc.controller;
+
+import com.spring6restmvc.model.Beer;
+import com.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    private final BeerService beerService;
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> listBeers() {
+        log.debug("Get Beers - in controller");
+        return beerService.listBeers();
+    }
+
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer gerBeerById(@PathVariable("beerId") UUID beerId) {
+        log.debug("Get Beer by Id " + beerId + " - in controller");
+        return beerService.getBeerById(beerId);
+    }
+}
--- spring-6-rest-mvc/src/main/java/com/spring6restmvc/controller/CustomerController.java
+++ spring-6-rest-mvc/src/main/java/com/spring6restmvc/controller/CustomerController.java
@@ -1,12 +1,9 @@
 package com.spring6restmvc.controller;
 
-import com.spring6restmvc.model.Beer;
 import com.spring6restmvc.model.Customer;
-import com.spring6restmvc.services.BeerService;
 import com.spring6restmvc.services.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,31 +15,20 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1")
-public class HomeController {
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
 
-    private final BeerService beerService;
     private final CustomerService customerService;
 
-    @RequestMapping(value = "/beer", method = RequestMethod.GET)
-    public List<Beer> listBeers() {
-        return beerService.listBeers();
-    }
-
-    @RequestMapping(value = "/beer/{beerId}", method = RequestMethod.GET)
-    public Beer gerBeerById(@PathVariable("beerId") UUID beerId) {
-        log.debug("Get Beer by Id - in controller");
-        return beerService.getBeerById(beerId);
-    }
-
-    @RequestMapping(value = "/customer", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Customer> listCostumer() {
+        log.debug("Get Customers - in controller");
         return customerService.getAllCustomers();
     }
 
-    @RequestMapping(value = "/customer/{customerId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{customerId}", method = RequestMethod.GET)
     public Customer gerCustomerById(@PathVariable("customerId") UUID customerId) {
-        log.debug("Get Beer by Id - in controller");
+        log.debug("Get Customer by Id "  + customerId + " - in controller");
         return customerService.getCustomerById(customerId);
     }
 }
